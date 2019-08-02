5db3cd3ec918b0d257b7ca7e9278a6739678d4d7
Abdul Rehman <abduls03357@gmail.com>
2019-08-02 16:07:56 +0500

    commit

--- app/src/main/java/com/example/timetoclose/UpgradeAccount.java
+++ app/src/main/java/com/example/timetoclose/UpgradeAccount.java
@@ -5,14 +5,32 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UpgradeAccount extends AppCompatActivity {
+
+    Button btnBackToHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upgrade_account);
 
+        swipe();
+
+        btnBackToHome= (Button)findViewById(R.id.btn_back_upgrade);
+        btnBackToHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UpgradeAccount.this,Home.class));
+                finish();
+            }
+        });
+
+    }
+
+    void swipe(){
         ConstraintLayout mLayout=(ConstraintLayout)findViewById(R.id.layout_upgrade_accunt);
 
         mLayout.setOnTouchListener(new OnSwipeTouchListener(UpgradeAccount.this) {
