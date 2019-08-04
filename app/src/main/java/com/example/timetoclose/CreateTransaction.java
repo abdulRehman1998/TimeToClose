0ca215b7ff7f539053f17b9ff662b7415be91186
Aniqurrehman042 <aniqurrehman042@gmail.com>
2019-08-04 18:33:55 +0500

    test

---
+++ app/src/main/java/com/example/timetoclose/CreateTransaction.java
@@ -0,0 +1,15 @@
+package com.example.timetoclose;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateTransaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_create_transaction);
+    }
+}
